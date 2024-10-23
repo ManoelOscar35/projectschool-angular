da153f1e2b64406f06eb38c9ddf70ff1b0efea2d
ManoelOscar35 <manoeloscar557@gmail.com>
2024-10-23 17:48:45 -0300

    Add Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ProjectSchool';
+
+  popText: any;
+  applyShadow: any;
+  isSmallScreen: any;
+  sidenavMode: any;
 }
